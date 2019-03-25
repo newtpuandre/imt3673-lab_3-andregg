b2ed921fdb2d7e0b2babbc04b78b1ffda76eb9b4
newtpuandre <andgun96@gmail.com>
2019-03-25 15:37:19 +0100

    Added acceleration formula

--- app/src/main/java/local/andregg/lab_3/MainActivity.java
+++ app/src/main/java/local/andregg/lab_3/MainActivity.java
@@ -1,13 +1,63 @@
 package local.andregg.lab_3;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private final float EarthGravity = 9.81f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this,sensor,sensorManager.SENSOR_DELAY_NORMAL);
+
     }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,sensor,sensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float height = 0;
+
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            float ACC = (float) Math.sqrt(x*x + y*y + z*z) - EarthGravity;
+
+            if (ACC >= 10) { //TODO: change to slider in preferences
+
+
+            }
+        }
+    }
+
+
 }
