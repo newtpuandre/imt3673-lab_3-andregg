68186bbab470df4bc855580c393ac4986ce4202c
newtpuandre <andgun96@gmail.com>
2019-03-26 17:05:36 +0100

    Implemented, sound on top. Count height + other fixes

--- app/src/main/java/local/andregg/lab_3/MainActivity.java
+++ app/src/main/java/local/andregg/lab_3/MainActivity.java
@@ -5,15 +5,35 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor sensor;
+    private Vibrator v;
+
+    private TextView currentHeight;
+    private TextView highscore;
+    private TextView throwStatus;
+    private MediaPlayer mp;
 
     private final float EarthGravity = 9.81f;
+    private boolean inAir = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +43,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this,sensor,sensorManager.SENSOR_DELAY_NORMAL);
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        currentHeight = findViewById(R.id.currentheight_txt);
+        highscore = findViewById(R.id.highscore_txt);
+        throwStatus = findViewById(R.id.throwstatus_txt);
+        mp = MediaPlayer.create(this, R.raw.popsound);
 
     }
 
@@ -36,6 +62,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.registerListener(this,sensor,sensorManager.SENSOR_DELAY_NORMAL);
     }
 
+
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
@@ -44,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float height = 0;
+            float biggestAcc;
 
             float x = event.values[0];
             float y = event.values[1];
@@ -52,18 +80,66 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
             float ACC = (float) Math.sqrt(x*x + y*y + z*z) - EarthGravity;
 
-            if (ACC >= 10) { //TODO: change to slider in preferences
+            if (ACC >= 40 && !inAir) { //TODO: change to slider in preferences
                 heightCalculation(ACC);
             }
         }
     }
 
-    public void heightCalculation(float acceleration){
-        float t = acceleration / EarthGravity;
-        float s = acceleration * t + 1/2 * -EarthGravity * (float) Math.pow(t,2);
+
+    public void heightCalculation(final float acceleration){
+        //Set textboxes etc
+        inAir = true;
+        v.vibrate(500);
+        final float t = acceleration / EarthGravity;
+        final float s = acceleration * t + 1/2 * -EarthGravity * (float) Math.pow(t,2);
         //Count to time and play sound
-        Log.d("app1", "" + s);
         //Calculate height and get high score if it is higher
+
+        updateText( (int) t * 1000, s);
+
+    }
+
+    private void updateText(final int maxtime, final float maxHeight) {
+        throwStatus.setText("In the air..");
+        new CountDownTimer((long) maxtime * 2, 100) {
+
+            float countNumber = ((maxHeight / maxtime)) * 100 ;
+            float curHeight;
+            boolean atTop = false;
+            public void onTick(long millisUntilFinished) {
+
+                if(!atTop) {
+                    curHeight += countNumber;
+                    currentHeight.setText(String.valueOf(curHeight));
+                    Log.d("app1", "countNumber " + countNumber + " " + maxHeight / maxtime);
+                }
+
+                if ((float) millisUntilFinished <= (maxtime) && !atTop) {
+                    Log.d("app1", "At the top" + millisUntilFinished);
+                    currentHeight.setText(String.valueOf(maxHeight));
+                    Log.d("app1", "" + maxHeight);
+                    throwStatus.setText("Reached the top!");
+                    mp.start();
+                    atTop = true;
+                }
+
+            }
+
+            public void onFinish() {
+                inAir = false;
+                throwStatus.setText("Landed... Throw again!");
+                Log.d("app1", "Bottom" );
+                v.vibrate(200);
+
+                float curHighscore = Float.valueOf(highscore.getText().toString());
+                Log.d("app1", "HIGHSCORE" + curHighscore);
+                if(maxHeight >= curHighscore) {
+                    highscore.setText(String.valueOf(maxHeight));
+                }
+            }
+        }.start();
+
     }
 
 }
